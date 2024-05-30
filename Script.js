46849a0f258ee862fc788f0b3803ac4ffa104403
Mehmet Said Unlu <mehmetkatza2001@gmail.com>
2024-05-30 21:52:40 +0200

    zekaiwebsite

---
+++ Script.js
@@ -0,0 +1,23 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('contactButton').addEventListener('click', function () {
+        window.location.href = 'Contact.html';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('project3').addEventListener('click', function () {
+        window.location.href = 'https://github.com/Israfil-Zekai-Yilmaz/Guess.git';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('project1').addEventListener('click', function () {
+        window.location.href = 'https://github.com/Israfil-Zekai-Yilmaz/BookLibrary.git';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('project2').addEventListener('click', function () {
+        window.location.href = 'https://github.com/Israfil-Zekai-Yilmaz/10FastFingerss.git';
+    });
+});
